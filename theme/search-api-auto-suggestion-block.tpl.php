294477683f8d8b981f3db336c7ae66660bc501ec
Ethan Han <ehan@dresourcesgroup.com>
2016-02-16 12:23:39 -0500

    Added search_api_auto_suggestion module.

---
+++ theme/search-api-auto-suggestion-block.tpl.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * @file
+ * Search api auto suggestion block theme file.
+ */
+  
+?>
+
+<?php if ($variables['original_term'] != NULL): ?>
+<div>
+  <p><?php print t('Showing results for !link.', array('!link' => l($variables['search_term'], 'search', array('query' => array('query' => $variables['search_term'], 'is_original' => TRUE))))); ?></p>
+  <p><?php print t('Search instead for !link?', array('!link' => l($variables['original_term'], 'search', array('query' => array('query' => $variables['original_term'], 'is_original' => TRUE))))); ?></p>
+</div>
+<?php endif; ?>
